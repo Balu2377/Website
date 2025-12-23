e638c0fe87110a59a607db7856e2cd151f075957
Manohar Javvaji <112223392+Balu2377@users.noreply.github.com>
2025-12-24 02:06:32 +0530

    Refactor toggleLanguage function and clean comments

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// script.js
 function toggleLanguage() {
     const body = document.body;
     const btn = document.getElementById('langBtn');
 
-    // Toggle the class on the body element
     body.classList.toggle('english-mode');
 
-    // Change the button text based on the mode
     if (body.classList.contains('english-mode')) {
         btn.innerText = "తెలుగు";
     } else {
